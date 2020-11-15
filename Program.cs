6f4547606eae374be624d3098335ba27468b3759
authentictorch25 <64377971+authentictorch25@users.noreply.github.com>
2020-11-16 01:37:43 +0530

    [Akash] Added program to add contact using er

--- Program.cs
+++ Program.cs
@@ -13,6 +13,21 @@ namespace AddressBookThread
             repository.GetContactsAddedInPeriod("2018-01-01", "2020-01-01");
             //UC 19
             repository.GetNumberOfContactsByCityOrState();
+            //UC20
+            AddressBookModel contactDetails = new AddressBookModel();
+            contactDetails.FirstName = "samrat";
+            contactDetails.LastName = "pandey";
+            contactDetails.Address = "Chennai";
+            contactDetails.City = "chennai";
+            contactDetails.State = "Karnataka";
+            contactDetails.Zip = 45895;
+            contactDetails.PhoneNumber = 654645465478;
+            contactDetails.Email = "viratkohli@gmail.com";
+            contactDetails.DateAdded = Convert.ToDateTime("2019-06-10");
+            contactDetails.AddressBookName = "BCCI";
+            contactDetails.ContactType = "CRICKETER";
+            contactDetails.TypeCode = "CRI";
+            Console.WriteLine(repository.AddContactDetailsIntoDataBase(contactDetails) ? "Contact added successfully" : "Contact was not added");
         }
     }
 }
